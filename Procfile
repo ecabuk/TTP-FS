a9d1a63a6a1aa82473bdece6ec087dbc93fc2bed
Evrim Cabuk <ecabuk@ecabuk.net>
2019-07-11 22:55:22 -0400

    Makes it heroku compatible

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app.ttp_fs.wsgi --log-file -
--- app/ttp_fs/settings_production.py
+++ app/ttp_fs/settings_production.py
@@ -1,3 +1,11 @@
+import django_heroku
 from .settings_common import *
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configure Django App for Heroku.
+django_heroku.settings(
+    locals(),
+    staticfiles=False,
+    test_runner=False
+)
